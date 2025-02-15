04794a7b8a04e2de64757fce97806b2f646f8e3c
woong <woong7361@gmail.com>
2025-02-15 11:04:59 +0900

    init: start prohject

---
+++ src/main/java/kr/co/polycube/backendtest/BackendTestApplication.java
@@ -0,0 +1,16 @@
+package kr.co.polycube.backendtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+            BackendTestApplication.class,
+            args
+        );
+    }
+
+}
---
+++ src/test/java/kr/co/polycube/backendtest/BackendTestApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.polycube.backendtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
